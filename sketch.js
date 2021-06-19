184aa5b540940a3628a0aba38d39c949ca33c108
ejay11 <ellen.jaquette@gmail.com>
2021-06-18 21:46:14 -0500

    grid and hover effect added

--- sketch.js
+++ sketch.js
@@ -1,14 +1,36 @@
 //Creating Divs
-alert("hi");
 
 function createDiv(){
     let x = 0;
-    let y = 15;
+    let y = 255;
     const container = document.querySelector('.container');
     while(x <= y){
         const square = document.createElement("div");
+        square.id = 'square';
         container.appendChild(square);
         x++;
     }
 }
 createDiv();
+
+const squares = document.querySelectorAll('#square');
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () =>{
+        square.style.backgroundColor = 'black';
+    });
+    
+    //square.addEventListener("mouseleave", () =>{
+        //square.style.backgroundColor = 'white';
+   // })
+});
+
+
+
+//function colorChange {
+   // square.style.backgroundColor = 'black';
+//}
+
+//function colorRemove{
+  //  square.style.backgroundColor = 'white';
+//}
