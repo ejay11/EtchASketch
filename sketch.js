f8e374f239589bfb6424398f5c67a3e84e7024a7
ejay11 <ellen.jaquette@gmail.com>
2021-06-28 10:36:07 -0500

    updated event listeners

--- sketch.js
+++ sketch.js
@@ -23,35 +23,34 @@ function changeGrid() {
     //Changing style for responsive grid - was going to attemp to insert (num / 2) into row template, but even style without variable isn't working
     document.querySelector("#grid").style.gridTemplateColumns = "repeat(4, 1fr);";
     document.querySelector("#grid").style.gridTemplateRows = "repeat(4, 1fr);";
-    
-      
+     
 
     //adding event listener to new divs
 
-    // document.querySelectorAll('.square').forEach(square => {
-    // square.addEventListener('mouseenter', event => {
-    //     square.style.backgroundColor = 'black';
-    // })
-    //  })
+    document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseenter', event => {
+       square.classList.add('black');
+    })
+    })
 
 }
 
 //Functions and Event Listener for squares/change colors
-//document.querySelectorAll('.square').forEach(square => {
-//square.addEventListener('mouseenter', event => {
-//square.style.backgroundColor = 'black';
- //})
-//})
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('mouseenter', event => {
+       square.classList.add('black');
+    })
+    })
 
 //new event listener - Not working
-document.addEventListener('mouseenter', () => {
-    let sketchpad = document.getElementByClass('div');
-    if (sketchpad.classList.contains('square')) {
-        sketchpad.classList.add('black');
+//document.addEventListener('mouseenter', () => {
+    //let sketchpad = document.getElementByClass('div');
+    //if (sketchpad.classList.contains('square')) {
+        //sketchpad.classList.add('black');
         //for(var i = 0; i < sketchpad.length; i++) {
           //  sketchpad[i].classList.add('black');
-    }
-})
+    //}
+//})
 
 //Reset Button
 document.querySelector('button').addEventListener("click", () => {
