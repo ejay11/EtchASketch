8890dd76be910bebf1a02e7eca843d3d97fb4f9e
ejay11 <ellen.jaquette@gmail.com>
2021-06-28 10:21:55 -0500

    work on grid styling

--- sketch.js
+++ sketch.js
@@ -11,15 +11,20 @@ function createDiv(num) {
     }
 }
 //creating initial grid 16x16
-//createDiv(16);
+createDiv(16);
 
 //function to change size of grid display to user input
 
 function changeGrid() {
     let num = prompt('How many columns do you want in your sketchpad? Enter a number 1-100');
-    //try to remove previous square divs. Not currently working//
 
     createDiv(num);
+
+    //Changing style for responsive grid - was going to attemp to insert (num / 2) into row template, but even style without variable isn't working
+    document.querySelector("#grid").style.gridTemplateColumns = "repeat(4, 1fr);";
+    document.querySelector("#grid").style.gridTemplateRows = "repeat(4, 1fr);";
+    
+      
 
     //adding event listener to new divs
 
@@ -32,27 +37,28 @@ function changeGrid() {
 }
 
 //Functions and Event Listener for squares/change colors
-document.querySelectorAll('.square').forEach(square => {
-square.addEventListener('mouseenter', event => {
-square.style.backgroundColor = 'black';
- })
-})
-
-//new event listener - Not working
-//document.querySelector('container').addEventListener('mouseenter', () => {
-    //let sketchpad = document.getElementById('grid');
-   // if (sketchpad.classList.contains('square')) {
-     //   sketchpad.classList.add('black');
-    //}
+//document.querySelectorAll('.square').forEach(square => {
+//square.addEventListener('mouseenter', event => {
+//square.style.backgroundColor = 'black';
+ //})
 //})
 
+//new event listener - Not working
+document.addEventListener('mouseenter', () => {
+    let sketchpad = document.getElementByClass('div');
+    if (sketchpad.classList.contains('square')) {
+        sketchpad.classList.add('black');
+        //for(var i = 0; i < sketchpad.length; i++) {
+          //  sketchpad[i].classList.add('black');
+    }
+})
+
+//Reset Button
 document.querySelector('button').addEventListener("click", () => {
     document.querySelectorAll('.square').forEach(square => {
-        //square.style.backgroundColor = 'white';
         var cleanSlate = document.getElementsByClassName("square");
         for(var i = 0; i < cleanSlate.length; i++) {
         cleanSlate[i].parentNode.removeChild(cleanSlate[i]);
-        //square.remove()
     }})
     changeGrid();
 })
