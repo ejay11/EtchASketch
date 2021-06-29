424617f032666ecf08d2c2e99130b2b9b57990d4
ejay11 <ellen.jaquette@gmail.com>
2021-06-29 09:39:12 -0500

    all working functions and responsive grid

--- sketch.js
+++ sketch.js
@@ -1,46 +1,43 @@
 //Function to create divs for the grid
 function createGrid(num) {
     let y = (num * num) - 1;
-    let grid = document.querySelector('#grid');
+    const gridContainer = document.querySelector('#gridContainer');
+    const gridElem = document.createElement('div');
+    gridElem.id = 'grid';
+    gridContainer.append(gridElem);
     for (let x = 0; x <= y; x++) {
         const square = document.createElement("div");
         square.classList.add('square');
-        grid.appendChild(square);
+        gridElem.appendChild(square);
     }
     document.querySelectorAll('.square').forEach(square => {
         square.addEventListener('mouseenter', event => {
-           square.classList.add('black');
+            square.classList.add('black');
         })
-        })
+    })
 }
 
-//function deleteGrid
+function deleteGrid() {
+    const grid = document.querySelector('#grid');
+    grid.remove()
+}
 
 
 function changeGrid() {
     let num = prompt('How many columns do you want in your sketchpad? Enter a number 1-100');
     //put in validation
+    deleteGrid();
     createGrid(num);
 
-    //Changing style for responsive grid - was going to attemp to insert (num / 2) into row template, but even style without variable isn't working
+    //Changing style for responsive grid
     let grid = document.querySelector("#grid");
-    let style = `repeat(${num}, 1fr`;
-    grid.setAttribute("style", `grid-template-columns: ${style}`);
-    grid.setAttribute("style", `grid-template-rows: ${style}`);
-    //document.querySelector("#grid").style.gridTemplateColumns = "repeat(4, 1fr);";
-    //document.querySelector("#grid").style.gridTemplateRows = "repeat(4, 1fr);";
+    let gridStyle = `repeat(${num}, 1fr);`;
+    grid.setAttribute('style', `grid-template-columns: ${gridStyle}; grid-template-rows: ${gridStyle}`);
 }
 
 
 // --- EXECUTE STUFF ---
 //Reset Button
-document.querySelector('button').addEventListener("click", () => {
-    document.querySelectorAll('.square').forEach(square => {
-        var cleanSlate = document.getElementsByClassName("square");
-        for(var i = 0; i < cleanSlate.length; i++) {
-        cleanSlate[i].parentNode.removeChild(cleanSlate[i]);
-    }})
-    changeGrid();
-})
+document.querySelector('button').addEventListener("click", changeGrid);
 
 createGrid(16);
